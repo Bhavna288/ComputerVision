999b4d4ba663cac0cb5e34d4126cc326a930c62e
Bhavna288 <bhavnatahelyani288@gmail.com>
2021-02-03 23:31:59 +0530

    Convert.py

--- convert.py
+++ convert.py
@@ -14,7 +14,7 @@ import os
 # %matplotlib inline
 
 # Read the image file
-image_path = os.path.join('handwriting.png')
+image_path = os.path.join('letter_.jpg')
 image_stream = open(image_path, "rb")
 
 # Get a client for the computer vision service
