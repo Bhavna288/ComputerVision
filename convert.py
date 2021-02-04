a6f755b13584f107816a213ae7f541f20550c9cd
Bhavna288 <bhavnatahelyani288@gmail.com>
2021-02-04 23:40:08 +0530

    File upload

--- convert.py
+++ convert.py
@@ -11,34 +11,36 @@ import os
 
 # %matplotlib inline
 
-# Read the image file
-image_path = os.path.join('numberplate.jpg')
-image_stream = open(image_path, "rb")
+def extractText():
 
-# Get a client for the computer vision service
-computervision_client = ComputerVisionClient(cog_endpoint, CognitiveServicesCredentials(cog_key))
+    # Read the image file
+    image_path = os.path.join('numberplate.jpg')
+    image_stream = open(image_path, "rb")
 
-# Submit a request to read printed text in the image and get the operation ID
-read_operation = computervision_client.read_in_stream(image_stream, raw=True)
-operation_location = read_operation.headers["Operation-Location"]
-operation_id = operation_location.split("/")[-1]
+    # Get a client for the computer vision service
+    computervision_client = ComputerVisionClient(cog_endpoint, CognitiveServicesCredentials(cog_key))
 
-# Wait for the asynchronous operation to complete
-while True:
-    read_results = computervision_client.get_read_result(operation_id)
-    if read_results.status not in [OperationStatusCodes.running]:
-        break
-    time.sleep(1)
+    # Submit a request to read printed text in the image and get the operation ID
+    read_operation = computervision_client.read_in_stream(image_stream, raw=True)
+    operation_location = read_operation.headers["Operation-Location"]
+    operation_id = operation_location.split("/")[-1]
 
-# If the operation was successfuly, process the text line by line
-if read_results.status == OperationStatusCodes.succeeded:
-    for result in read_results.analyze_result.read_results:
-        for line in result.lines:
-            print(line.text)
+    # Wait for the asynchronous operation to complete
+    while True:
+        read_results = computervision_client.get_read_result(operation_id)
+        if read_results.status not in [OperationStatusCodes.running]:
+            break
+        time.sleep(1)
 
-# Open image and display it.
-print('\n')
-fig = plt.figure(figsize=(12,12))
-img = Image.open(image_path)
-plt.axis('off')
-plt.imshow(img)
+    # If the operation was successfuly, process the text line by line
+    if read_results.status == OperationStatusCodes.succeeded:
+        for result in read_results.analyze_result.read_results:
+            for line in result.lines:
+                print(line.text)
+
+    # Open image and display it.
+    print('\n')
+    fig = plt.figure(figsize=(12,12))
+    img = Image.open(image_path)
+    plt.axis('off')
+    plt.imshow(img)
--- tutorial.py
+++ tutorial.py
@@ -1,21 +1,33 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request, session
+from convert import extractText
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
+app.secret_key = "army_bts"
 
-@app.route("/<name>")
-def home(name):
-    return render_template("index.html", content = ["Namjoon", "Seokjin", "Yoongi", "Hoseok", "Jimin", "Taehyung", "Jungkook"])
+@app.route("/", methods=["POST", "GET"])
+def home():
+    if request.method == "POST":
+        uploaded_file = request.files["file"]
+        session["uploaded_file"] = uploaded_file.filename
+        uploaded_file.save(secure_filename(uploaded_file.filename))
+        return redirect(url_for("result"))
+    else:
+        return render_template("index.html")
 
-# @app.route("/<name>")
-# def user(name):
-#     return "Hello {}".format(name)
+@app.route("/result")
+def result():
+    return render_template("result.html")
 
-# @app.route("/admin/")
-# def admin():
-#     return redirect(url_for("user", name="Admin!"))
+@app.route("/user")
+def user():
+    if "user" in session:
+        return "<h1>{}</h1>".format(session["user"])
+    else:
+        return redirect(url_for("home"))
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
 
 # import sys
 # print(sys.path)
